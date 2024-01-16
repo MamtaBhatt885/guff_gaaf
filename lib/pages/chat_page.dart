2958b5c0ca6c2b59e7feae89f272627fd22c3e90
MamtaBhatt885 <610mamtabhatt@gmail.com>
2024-01-16 21:49:54 +0545

    was adding send and receive message functionality but it's not working properly but was successful to add text. The only problem is it's not showing the text on ui screen

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,12 +1,106 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:guff_gaaf/component/my_textfield.dart';
+import 'package:guff_gaaf/services/auth/auth_service.dart';
+import 'package:guff_gaaf/services/chat/chat_services.dart';
+
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
-  const ChatPage({super.key, required this.receiverEmail                                                                                                                        });
+  final String receiverID;
+
+  ChatPage({super.key, required this.receiverEmail, required this.receiverID});
+
+//text controller
+  final TextEditingController _messageController = TextEditingController();
+
+//chat & auth services
+  final ChatServices _chatService = ChatServices();
+  final AuthService _authService = AuthService();
+
+// send message
+  void sendMessage() async {
+    // if there is something inside the textfield
+    if (_messageController.text.isNotEmpty) {
+      //send the message
+      await _chatService.sendMessage(receiverID, _messageController.text);
+
+      //clear the text controller
+      _messageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(receiverEmail),),
+        appBar: AppBar(
+          title: Text(receiverEmail),
+        ),
+        body: Column(
+          children: [
+//display all messages
+            Expanded(
+              child: _buildMessageList(),
+            ),
+
+//user input
+_buildUserInput(),
+
+          ],
+        ));
+  }
+
+  //build message List
+  Widget _buildMessageList() {
+    String _senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+        stream: _chatService.getMessages(receiverID, _senderID),
+        builder: (context, snapshot) {
+//error
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+
+//loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading...");
+          }
+
+//return list view
+          return ListView(
+            children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+          );
+        },
+        );
+  }
+
+  //build message item
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  //build message input
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+//textfield should take up most of the space
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: "Type a message",
+            obscureText: false,
+          ),
+        ),
+
+//send button
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(Icons.arrow_upward),
+        ),
+      ],
     );
   }
 }
--- lib/services/chat/chat_services.dart
+++ lib/services/chat/chat_services.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:guff_gaaf/models/message.dart';
 
-class ChatServices{
-//get instance of firestore
-final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+class ChatServices {
+//get instance of firestore & auth
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 //get user stream
 /*
 List<Map<String,dynamic> = 
@@ -19,19 +21,57 @@ List<Map<String,dynamic> =
 ]
 This is what we call as list of maps.
 */
-Stream<List<Map<String,dynamic>>>getUsersStream(){
-return _firestore.collection("Users").snapshots().map((snapshot) {
-  return snapshot.docs.map((doc){
+  Stream<List<Map<String, dynamic>>> getUsersStream() {
+    return _firestore.collection("Users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
 //go through each individual user
-final user = doc.data();
+        final user = doc.data();
 
 //return user;
-return user;
-  }).toList();
-});
-}
+        return user;
+      }).toList();
+    });
+  }
 
 //send message
+  Future<void> sendMessage(String receiverID, message) async {
+    //get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    //create a new message
+    Message newMessage = Message(
+        senderID: currentUserEmail,
+        senderEmail: currentUserID,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp);
+
+    //construct chat room ID for the two users(sorted to ensure uniqueness)
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); //sort the ids(this ensure the chatroomID is the same for any 2 people)
+    String chatroomID = ids.join('_');
+
+    //add new message to database
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatroomID)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
 
 //get messages
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+    //construct a chatroom ID for the two users
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+    return _firestore
+        .collection("chat_room")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
