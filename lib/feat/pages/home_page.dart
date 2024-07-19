50de0a00f50a3a339e27d986f50fb866c6cbce93
Mamta Bhatt <610mamtabhatt@gmail.com>
2024-07-19 15:47:48 +0545

    Signout button with functionality

--- lib/feat/pages/home_page.dart
+++ lib/feat/pages/home_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:guff_gaaf/feat/auth/auth_services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout(){
+//get auth service
+  final _auth = AuthService();
+  _auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
       appBar: AppBar(
-        title: Text("Home Page"),
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Center(child: Text("Home")),
+actions: [
+  IconButton(onPressed: logout, icon:Icon(Icons.logout) )
+],
+
       ),
     );
   }
